230650d83fbf8951ff60a94d5d0a75b6bb2e2f17
kloudritika <ritika.nigam@klouddata.com>
2017-09-07 13:14:30 +0530

    Add New Topic

---
+++ src/app/_services/dialogs/dialogs.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DialogsService } from './dialog.service';
+import { SharedModule } from '../../_shared/_shared.module';
+import { FormsModule, ReactiveFormsModule, NgModel } from '@angular/forms';
+
+import { EditCalendarDialog } from './edit.calendar.dialog.component';
+import { ViewConflictDialog } from './view.conflict.dialog.component';
+import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule
+  ],
+  exports: [EditCalendarDialog, DeleteDialogComponent],
+  declarations: [EditCalendarDialog, ViewConflictDialog, DeleteDialogComponent],
+  providers: [
+    DialogsService,
+  ],
+  entryComponents: [
+    EditCalendarDialog,
+    ViewConflictDialog,
+    DeleteDialogComponent
+  ],
+})
+export class DialogsModule { }
