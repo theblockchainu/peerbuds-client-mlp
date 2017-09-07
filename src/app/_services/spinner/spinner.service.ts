d4d8b8f1a4ddf92ffcc8d474a8f6df72b6373aad
kloudritika <ritika.nigam@klouddata.com>
2017-09-07 15:39:01 +0530

    Spinner as a service

---
+++ src/app/_services/spinner/spinner.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/delay';
+
+@Injectable()
+export class SpinnerService {
+  spinnerState: BehaviorSubject<boolean>;
+  private options;
+
+  constructor() {
+    this.spinnerState = new BehaviorSubject(true);
+  }
+
+  getSpinnerState() {
+    return this.spinnerState.asObservable();
+  }
+
+  setSpinnerState(value: boolean) {
+      this.spinnerState.next(value);
+  }
+
+}
