ac693ddae8fefb4ec6fe92af27f79bec8fc18fcc
Abhijeet Chakraborty <abhijeet1abhijeet@gmail.com>
2017-09-07 13:20:59 +0530

    rerouted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { AppConfig } from '../app.config';
 import { Component, OnInit } from '@angular/core';
-import { Router, Params, NavigationStart } from '@angular/router';
+import { Router, Params, NavigationStart, ActivatedRoute } from '@angular/router';
 import { CookieUtilsService } from '../_services/cookieUtils/cookie-utils.service';
 import { CollectionService } from '../_services/collection/collection.service';
 
@@ -14,6 +14,7 @@ export class HomeComponent implements OnInit {
   public activeTab: string;
   public userId: string;
   constructor(public router: Router,
+    private activatedRoute: ActivatedRoute,
     private cookieUtilsService: CookieUtilsService,
     private _collectionService: CollectionService,
     private appConfig: AppConfig) {
