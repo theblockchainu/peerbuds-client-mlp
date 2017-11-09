c3278dee823edaa61fd080cce4d64bef5ab8d980
KloudRitika <ritika.nigam@klouddata.com>
2017-11-09 21:21:49 +0530

    Workshop fixes (#104)
    * Image uploader and supplement Url changes
    
    * Ongoing Media Delete fixed | Fixed few Demo Bugs | Ongoing Reviews in View Profile
    
    * Solved:
    1. Review from learners - profile page
    2. Edit Calendar - Repeat Till no of sessions fixed
    
    * Removing debuggers
    
    * Some missed updates
    
    * Showing List of New Cohorts and Fixed recurrences issue for all scenarios
    
    * Fixes for Recurring Workshop
    
    * Workshop Recurring: Conflict, Delete, Recurring Logic, style fixes
    
    * Duplicate code
    
    * Add/Delete calendar RefreshView, show the latest month as per the last date not the current month, style issues
    
    * Removing debugger
    
    * Style fixes, edit calendar, delete cohort

--- src/app/workshop/dialogs/view-conflict-dialog/view-conflict-dialog.component.ts
+++ src/app/workshop/dialogs/view-conflict-dialog/view-conflict-dialog.component.ts
@@ -3,6 +3,7 @@ import { MdDialogRef, MD_DIALOG_DATA, MdButtonModule } from '@angular/material';
 import {
   FormGroup, FormArray, FormBuilder, FormControl, AbstractControl, Validators
 } from '@angular/forms';
+import _ from 'lodash';
 
 @Component({
     selector: 'app-view-conflict-dialog',
@@ -13,23 +14,41 @@ import {
 export class ViewConflictDialogComponent implements OnInit {
 
     public conflicts: FormGroup;
+    public droppedCalendars = [];
+    private proceedButtonText = 'Proceed with conflicts';
 
     constructor(public dialogRef1: MdDialogRef<ViewConflictDialogComponent>,
     @Inject(MD_DIALOG_DATA) public data: any,
     private _fb: FormBuilder) { }
 
     ngOnInit() {
-        console.log(this.data);
         this.conflicts = this._fb.group({
         });
     }
 
-    public continueAnyways() {
-        this.dialogRef1.close();
+    public undoDeleteCalendar(i) {
+        this.data.conflicts[i]['markedForDelete'] = false;
+        this.getProceedButtonText();
     }
 
-    public dropCalendar(startDate, endDate) {
-        this.dialogRef1.close({startDate, endDate});
+    public deleteCalendar(i) {
+        this.data.conflicts[i]['markedForDelete'] = true;
+        this.getProceedButtonText();
     }
+
+    public closeConflictDialog() {
+        this.dialogRef1.close(this.data.conflicts);
+    }
+
+    public getProceedButtonText() {
+        let forDelete = _.every(this.data.conflicts, ['markedForDelete', true]);
+        if (forDelete) {
+            this.proceedButtonText = 'Proceed';
+        }
+        else {
+            this.proceedButtonText = 'Proceed with remaining conflicts';
+        }
+    }
+
 
 }
