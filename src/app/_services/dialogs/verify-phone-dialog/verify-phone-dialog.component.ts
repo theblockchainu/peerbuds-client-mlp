0a8135f7246976a1cdbe08b42c29a09872f278e9
Sahana <sahana@Sahana.local>
2017-11-10 04:07:49 +0530

    css

--- src/app/_services/dialogs/verify-phone-dialog/verify-phone-dialog.component.ts
+++ src/app/_services/dialogs/verify-phone-dialog/verify-phone-dialog.component.ts
@@ -36,9 +36,9 @@ export class VerifyPhoneDialogComponent implements OnInit {
     public dialogRef: MdDialogRef<VerifyPhoneDialogComponent>,
     public _cookieUtilsService: CookieUtilsService,
     @Inject(MD_DIALOG_DATA) public data: any) {
-      this.activatedRoute.params.subscribe(params => {
-      });
-      this.userId = _cookieUtilsService.getValue('userId');
+    this.activatedRoute.params.subscribe(params => {
+    });
+    this.userId = _cookieUtilsService.getValue('userId');
   }
 
   ngOnInit() {
@@ -57,26 +57,31 @@ export class VerifyPhoneDialogComponent implements OnInit {
 
   continue(p) {
     this.step = p;
-   console.log('phone dialog opened');
-   this._profileService
-   .updatePeer(this.userId, {'phone': this.peer.controls['phone'].value})
-    .subscribe();
+    console.log('phone dialog opened');
+    this._profileService
+      .updatePeer(this.userId, { 'phone': this.peer.controls['phone'].value })
+      .subscribe();
+    console.log('phone dialog opened 2');
     if (p === 3) {
+      console.log('phone dialog opened 3');
       //this.peer.controls['phone'].setValue(this.phone);
       this.sendOTP();
-     }
+      console.log('phone dialog opened 4');
+    }
   }
 
   public sendOTP() {
     this._profileService.sendVerifySms(this.peer.controls.phone.value)
       .subscribe();
+    console.log('otp sent');
   }
 
   public resendOTP(message: string, action) {
     this._profileService.sendVerifyEmail(this.userId, this.peer.controls.phone.value)
       .subscribe((response) => {
-        this.snackBar.open('Code Resent', 'OK'); });
-    }
+        this.snackBar.open('Code Resent', 'OK');
+      });
+  }
 
   verifyPhone() {
     //this.peer.controls['phone'].setValue(this.phone);
@@ -90,4 +95,3 @@ export class VerifyPhoneDialogComponent implements OnInit {
       });
   }
 }
-
