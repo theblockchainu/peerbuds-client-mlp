9f304cfb24edd3264f374763af845a070e38abbe
donatedTunic1290 <connect@aakashbansal.com>
2018-02-16 12:47:32 +0530

    - Account approved cookie gets updated from socket now. - TODO: refresh the active page and show a toast to user that account is approved.

--- src/app/_services/cookieUtils/cookie-utils.service.ts
+++ src/app/_services/cookieUtils/cookie-utils.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import * as moment from 'moment';
 
 @Injectable()
 export class CookieUtilsService {
 
-  constructor(private _cookieService: CookieService) { }
+  constructor(private _cookieService: CookieService) {
+  }
 
   public getValue(key: string) {
     const cookieValue = this._cookieService.get(key).split(/[ \:.]+/);
-    return cookieValue[1];
+    console.log('getting cookie value of ' + key + ' as : ' + cookieValue);
+    return cookieValue.length > 1 ? cookieValue[1] : cookieValue[0];
   }
+
+    public setValue(name: string, value: string) {
+        this._cookieService.delete(name, '/', 'localhost');
+        this._cookieService.set(name, value, moment().add(2, 'days').toDate(), '/', 'localhost');
+    }
+
 }
